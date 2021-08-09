76f93e0f6922403d4ec6aa3712bb740dbe88bf4c
tommycp96 <tommycp8@gmail.com>
2021-08-09 19:36:00 +0700

    More Automatic Contet Creation

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -1,12 +1,22 @@
 import React, { useReducer } from 'react'
 
-export default (reducer, action, initialState) => {
+export default (reducer, actions, initialState) => {
   const Context = React.createContext()
 
   const Provider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState)
 
-    return <Context.Provider value={state}>{children}</Context.Provider>
+    // action === { addBlogPost: (dispatch) => { return () => {} } }
+    const boundActions = {}
+    for (let key in actions) {
+      boundActions[key] = actions[key](dispatch)
+    }
+
+    return (
+      <Context.Provider value={{ state, ...boundActions }}>
+        {children}
+      </Context.Provider>
+    )
   }
 
   return { Context, Provider }
